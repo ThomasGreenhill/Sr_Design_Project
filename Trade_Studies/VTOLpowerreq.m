6feff8f5549b0b20a3b88d592eb3a2652a076755
Xuchang Tang <xuctang@ucdavis.edu>
2021-02-09 23:35:11 -0800

    Pasted & Debugged by XTang
    Written by TGreenhill, pasted by XTang in this folder to be called on propeller power requirement analysis.

--- Trade_Studies/VTOLpowerreq.m
+++ Trade_Studies/VTOLpowerreq.m
@@ -1,14 +1,14 @@
 %% VTOLpowerreq.m
-% Provides figures for required power of VTOL aircraft in hover configuration
-%   to hover.
+% Provides figures for required power of VTOL aircraft
 %
 %   Inputs:
 %       m: Aircraft mass
+%       gfactor: Loading factor (1 = hovering)
+%       numMotor = Number of motors
 %       
-%           
 %
 %   Outputs: 
-%       {}
+%       Tm = Thrust required by each motor (equally distributed)
 %
 %   Calls:
 %       {none}
@@ -17,15 +17,16 @@
 %       1.
 %
 %   History:
+%           2/9/2021, X.Tang
 %       
 %
 
-function [] = VTOLpowerreq(m, gfactor, nummot )
+function [Tm] = VTOLpowerreq(m, gfactor, numMotor)
     g = 9.81; 
 
     Freq = m*gfactor*g;
 
-    Fprops = Freq/nummot; 
+    Fprops = Freq/numMotor; 
 
 
 end
