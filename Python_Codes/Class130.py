c59c1caa55a6078cc9683aeed33cfe3f2e6fbd2f
xuctang <xuctang@ucdavis.edu>
2021-02-27 14:32:54 -0800

    02.27.2021: XT. Code improvement
    Added error catching in std_atm.
    Improved accessibility of class AtmData.

--- Python_Codes/Class130.py
+++ Python_Codes/Class130.py
@@ -1,5 +1,5 @@
-
 import numpy
+import std_atm
 
 '''
 Useful Python classes in senior design project
@@ -24,40 +24,115 @@ History:
         02.16.2021, Added class for H2 fuel cells. TVG
 '''
 
+
 # Atmospheric information
 class AtmData:
     # In either base SI of British units
-    def __init__(self, vel, temp, pres, dens, visc, k, R, is_SI):
-        self.dens = None
-        self.vel = vel               # freestream velocity (m/s) or (ft/s)
-        self.temp = temp             # static temperature (K) or (R)
-        self.pres = pres             # static pressure (Pa) or (psia)
-        self.dens = dens             # air density (kg/m^3) or (slug/ft^3)
-        self.visc = visc             # air viscosity (N·s/m^2) or (lbf·s/ft^2)
-        self.k = k                   # specific heat ratio
-        self.R = R                   # gas constant of air [J/(kg·K)] or [(ft·lbf)/(lbm·R)]
-        self.is_SI = is_SI           # is_SI = True for SI units, is_SI = False for British units
+    def __init__(self, vel, h, is_SI):
+        self.alt = h    # altitude (m) or (ft)
+        self.vel = vel  # freestream velocity (m/s) or (ft/s)
+        self.is_SI = is_SI  # is_SI = True for SI units, is_SI = False for British units
+        temp, pres, dens, c_sound, visc, g = std_atm.std_atm(h, is_SI)
+        self.temp = temp  # static temperature (K) or (R)
+        self.pres = pres  # static pressure (Pa) or (psia)
+        self.dens = dens  # air density (kg/m^3) or (slug/ft^3)
+        self.visc = visc  # air viscosity (N·s/m^2) or (lbf·s/ft^2)
+        self.sound_speed = c_sound  # speed of sound (m/s) or (ft/s)
+        self.grav = g  # gravitational acceleration (m/s^2) or (ft/s^2)
+
+    def expand(self, k, R) -> object:
+        """
+        :param k: specific heat ratio
+        :param R: gas constant
+        :return: None
+        """
+        self.k = k  # specific heat ratio
+        self.R = R  # gas constant of air [J/(kg·K)] or [(ft·lbf)/(lbm·R)]
+        return
+
+    def print(self) -> None:
+        """
+        Prints out the existing attributes and values of AtmData class
+        :return:
+        """
+        num_round: int = 5
+        if self.is_SI:
+            print("==========================================")
+            print("At the height of {} m".format(round(self.alt, num_round)))
+            print("==========================================")
+            print("Air speed      (m/s)        :    {}".format(round(self.vel, num_round)))
+            print("Temperature    (K)          :    {}".format(round(self.temp, num_round)))
+            print("Pressure       (Pa)         :    {}".format(round(self.pres, num_round)))
+            print("Density        (kg/m^3)     :    {}".format(round(self.dens, num_round * 2)))
+            print("Viscosity      (Ns/m^2)     :    {}".format(round(self.visc, num_round * 2)))
+            print("Sound speed    (m/s)        :    {}".format(round(self.sound_speed, num_round)))
+            print("Grav. accel    (m/s^2)      :    {}".format(round(self.grav, num_round)))
+            print("==========================================")
+        else:
+            print("==========================================")
+            print("At the height of {} ft".format(round(self.alt, num_round)))
+            print("==========================================")
+            print("Air speed      (ft/s)       :    {}".format(round(self.vel, num_round)))
+            print("Temperature    (R)          :    {}".format(round(self.temp, num_round)))
+            print("Pressure       (psia)       :    {}".format(round(self.pres, num_round)))
+            print("Density        (slug/ft^3)  :    {}".format(round(self.dens, num_round * 2)))
+            print("Viscosity      (lbfs/ft^2)  :    {}".format(round(self.visc, num_round * 2)))
+            print("Sound speed    (ft/s)       :    {}".format(round(self.sound_speed, num_round)))
+            print("Grav. accel    (ft/s^2)     :    {}".format(round(self.grav, num_round)))
+            print("==========================================")
+        return
+
+    def convert(self) -> object:
+        """
+        Converts to the other unit system (SI to imperial or imperial to SI)
+        :return: None
+        """
+        # All convertion factors from SI to imperial
+        temp_conv = 1.8
+        pres_conv = 0.000145038
+        dens_conv = 0.00194032
+        m2ft = 3.28084
+        visc_conv = 0.224809 / (m2ft ** 2)
+        if self.is_SI:
+            self.is_SI = False
+            self.temp *= temp_conv
+            self.pres *= pres_conv
+            self.dens *= dens_conv
+            self.visc *= visc_conv
+            self.sound_speed *= m2ft
+            self.vel *= m2ft
+            self.grav *= m2ft
+        else:
+            self.is_SI = True
+            self.temp /= temp_conv
+            self.pres /= pres_conv
+            self.dens /= dens_conv
+            self.visc /= visc_conv
+            self.sound_speed /= m2ft
+            self.vel /= m2ft
+            self.grav /= m2ft
+        return
 
 
 # Propeller information
 class Propeller:
     # All in base SI units
-    def __init__(self, radius, RPM, eta_P, CP, CT, CQ, Cl = 0.4, chord = 1, numB = 3, alp0 = 0,
-                 alpha = None, beta = None, theta = None, phi = None):
-        self.radius = radius       # propeller radius (m)
-        self.RPM = RPM             # rotation per minute
-        self.eta_P = eta_P         # propeller efficiency
-        self.CP = CP               # power coeff.
-        self.CT = CT               # thrust coeff.
-        self.CQ = CQ               # torque coeff.
-        self.Cl = Cl               # lift coeff. distr.
-        self.chord = chord         # chord distr. (m)
-        self.numB = numB           # number of blades
-        self.alp0 = alp0           # zero-lift AoA (rad)
-        self.alpha = alpha         # AoA distr. (rad)
-        self.beta = beta           # pitch angle distr. (rad)
-        self.theta = theta         # induced angle distr. (rad)
-        self.phi = phi             # blade angle distr. (rad)
+    def __init__(self, radius, RPM, eta_P, CP, CT, CQ, Cl=0.4, chord=1, numB=3, alp0=0,
+                 alpha=None, beta=None, theta=None, phi=None):
+        self.radius = radius  # propeller radius (m)
+        self.RPM = RPM  # rotation per minute
+        self.eta_P = eta_P  # propeller efficiency
+        self.CP = CP  # power coeff.
+        self.CT = CT  # thrust coeff.
+        self.CQ = CQ  # torque coeff.
+        self.Cl = Cl  # lift coeff. distr.
+        self.chord = chord  # chord distr. (m)
+        self.numB = numB  # number of blades
+        self.alp0 = alp0  # zero-lift AoA (rad)
+        self.alpha = alpha  # AoA distr. (rad)
+        self.beta = beta  # pitch angle distr. (rad)
+        self.theta = theta  # induced angle distr. (rad)
+        self.phi = phi  # blade angle distr. (rad)
 
 
 # Airfoil information
@@ -65,40 +140,42 @@ class Airfoil:
     # All in base SI units
     # Will possibly be used with XFOIL
     def __init__(self, name, chord, alp0, alpha, Cla, Cl_max, Cl, Cd, Cm):
-        self.name = name             # name
-        self.chord = chord           # chord length list (m)
-        self.alp0 = alp0             # zero lift AoA (rad)
-        self.alpha = alpha           # AoA list
-        self.Cla = Cla               # lift curve slope
-        self.Cl_max = Cl_max         # max Cl
-        self.Cl = Cl                 # Cl list w.r.t. alpha
-        self.Cd = Cd                 # Cd list w.r.t. alpha
-        self.Cm = Cm                 # Cm list w.r.t. alpha
+        self.name = name  # name
+        self.chord = chord  # chord length list (m)
+        self.alp0 = alp0  # zero lift AoA (rad)
+        self.alpha = alpha  # AoA list
+        self.Cla = Cla  # lift curve slope
+        self.Cl_max = Cl_max  # max Cl
+        self.Cl = Cl  # Cl list w.r.t. alpha
+        self.Cd = Cd  # Cd list w.r.t. alpha
+        self.Cm = Cm  # Cm list w.r.t. alpha
 
 
 # Wing information
 class Wing:
     # All in base SI units
     def __init__(self, area, span, e, alpha, chord, c_bar, CL, CL_max, CD, CD_0, airfoil):
-        self.area = area                # wing area (m^2)
-        self.span = span                # wing span (m)
-        self.chord = chord              # chord distr. (m)
-        self.c_bar = c_bar              # average chord length (m)
-        self.e = e                      # Oswald's span efficiency ratio
-        self.alpha = alpha              # wing AoA
-        self.CL = CL                    # wing 3D CL
-        self.CL_max = CL_max            # wing stall CL
-        self.CD = CD                    # wing 3D CD
-        self.CD_0 = CD_0                # zero lift CD
-        self.airfoil = airfoil          # wing sectional airfoil type list
-        self.AR = span ** 2 / area      # wing aspect ratio
+        self.area = area  # wing area (m^2)
+        self.span = span  # wing span (m)
+        self.chord = chord  # chord distr. (m)
+        self.c_bar = c_bar  # average chord length (m)
+        self.e = e  # Oswald's span efficiency ratio
+        self.alpha = alpha  # wing AoA
+        self.CL = CL  # wing 3D CL
+        self.CL_max = CL_max  # wing stall CL
+        self.CD = CD  # wing 3D CD
+        self.CD_0 = CD_0  # zero lift CD
+        self.airfoil = airfoil  # wing sectional airfoil type list
+        self.AR = span ** 2 / area  # wing aspect ratio
         self.CD_i = CL ** 2 / (e * Wing.AR * numpy.pi)
 
 
 # Fuel cell information
 class FuelCell:
     # All in base SI units
-    def __init__(self, name, rated_power, min_voltage, max_voltage, max_current, cell_dimensions, cell_weight, coolant_dimensions, coolant_weight, air_dimensions, air_weight, oxidant, fuel_flow_rate, fuel_efficiency, sound_level):
+    def __init__(self, name, rated_power, min_voltage, max_voltage, max_current, cell_dimensions, cell_weight,
+                 coolant_dimensions, coolant_weight, air_dimensions, air_weight, oxidant, fuel_flow_rate,
+                 fuel_efficiency, sound_level):
         self.name = name
         self.rated_power = rated_power
         self.min_voltage = min_voltage
@@ -159,5 +236,13 @@ class Sizing:
         self.time_hover_climb = time_hover_climb
         self.time_cruise = time_cruise
         self.time_hover_descent = time_hover_descent
-        
 
+
+if __name__ == '__main__':
+    is_SI = True
+    vel = 300
+    h = 0
+    atm = AtmData(vel, h, is_SI)
+    atm.convert()
+    atm.expand(1.4, 53.35)
+    atm.print()
