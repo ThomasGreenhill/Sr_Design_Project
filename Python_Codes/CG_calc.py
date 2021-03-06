bc6ba3bfc45b5f770bdd446caa6b2019a986748d
michael7452 <michaelpuso@gmail.com>
2021-03-05 23:06:13 -0800

    Updated CG_calc and OpenVSP Model
    3.5.21 MP. Revised Xuchang's model to lower wingspan and reduce motor nacelle sizes. Used proper tail sizing. (CG_calc DOES NOT HAVE UPDATED VALUES!!)

--- Python_Codes/CG_calc.py
+++ Python_Codes/CG_calc.py
@@ -20,18 +20,53 @@ def FindCG(objs):
     yCOM = sum([obj.y*obj.mass for obj in objs])/total_mass
     zCOM = sum([obj.z*obj.mass for obj in objs])/total_mass
     
-    return (xCOM, yCOM, zCOM)
+    return (total_mass, xCOM, yCOM, zCOM)
 
 if __name__ == "__main__":
     
-    # Simple test with two arbitrary objects "a" and "b"
+    # Sample for our aircraft:
+        
+    # Motors
+    CanardMotor   = MassObj(12*2,-2.1,0,0)
+    InboardMotor  = MassObj(12*2,1.38,0,0)
+    MiddleMotor   = MassObj(12*2,1.7,0,0)
+    OutboardMotor = MassObj(12*2,2,0,0)
+    MotorList = [CanardMotor, InboardMotor, MiddleMotor, OutboardMotor]
     
-    a = MassObj(1,0,0,0)
-    b = MassObj(1,2,3,4)
-    objs = [a,b]
+    # Motor Controllers
+    CanardMotorCon   = MassObj(8*2,-2.1-0.0762,0,0)
+    InboardMotorCon  = MassObj(8*2,1.38-0.0762,0,0)
+    MiddleMotorCon   = MassObj(8*2,1.7-0.0762,0,0)
+    OutboardMotorCon = MassObj(8*2,2-0.0762,0,0)
+    MotorConList = [CanardMotorCon, InboardMotorCon, MiddleMotorCon, OutboardMotorCon]
     
+    # Rotors
+    CanardRotor   = MassObj(2,-2.1+0.4,0,0)
+    InboardRotor  = MassObj(2,1.38+1,0,0)
+    MiddleRotor   = MassObj(2,1.7+1,0,0)
+    OutboardRotor = MassObj(2,2+1,0,0)
+    RotorList = [CanardRotor, InboardRotor, MiddleRotor, OutboardRotor]
+    
+    # Main Wing
+    MainWing = MassObj(240,0.761,0,0)
+    
+    # Empennage
+    HorzTail = MassObj(46,5.44,0,0)
+    VertTail = MassObj(20.98,5.385,0,0)
+    Empennage = [HorzTail, VertTail]
+    
+    # Canard
+    Canard = MassObj(8.45,-2.17,0,0)
+    
+    # Passengers+Seats
+    FrontPax = MassObj(88.45,-0.937,0,0)
+    RearPax1 = MassObj(88.45,0.251,0,0)
+    RearPax2 = MassObj(88.45,0.251,0,0)
+    PaxAndSeats = [FrontPax, RearPax1, RearPax2]
+    
+    objs = [*MotorList, *MotorConList, *RotorList, MainWing, *Empennage, \
+            Canard, *PaxAndSeats]
     CG_pos = FindCG(objs)
     
-    print("xCOM: {}".format(CG_pos[0]))
-    print("yCOM: {}".format(CG_pos[1]))
-    print("zCOM: {}".format(CG_pos[2]))
+    print("Mass: {}".format(CG_pos[0]))
+    print("xCOM: {}".format(CG_pos[1]))
