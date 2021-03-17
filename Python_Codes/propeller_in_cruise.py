788d91c0ef33be3b8d1b36dd3416de38364f3e4f
Xuchang Tang <xuctang@ucdavis.edu>
2021-03-17 01:55:22 -0700

    03.17.2021, XT. range and endurance

--- Python_Codes/propeller_in_cruise.py
+++ Python_Codes/propeller_in_cruise.py
@@ -1,8 +1,8 @@
 import numpy
-from Class130 import AtmData, Propeller, Wing
 
 
 def propeller_in_cruise(W_initial, W_final, c_p, AtmData, Propeller, Wing, const_logic):
+    # return R, E
     '''
         Based on EAE 130A lecture slide on Jan 27, 2021
         Computes the range and endurance of a propeller-thrusted, fuel-burning, air-breathing airplane cruise
@@ -16,7 +16,8 @@ def propeller_in_cruise(W_initial, W_final, c_p, AtmData, Propeller, Wing, const
                             must only have 2 True and 1 False
 
         Outputs:
-            {none}
+                R: range
+                E: endurance
 
         Notes:
 
@@ -59,7 +60,7 @@ def propeller_in_cruise(W_initial, W_final, c_p, AtmData, Propeller, Wing, const
         a2 = numpy.sqrt(1 / (numpy.pi * Wing.e * Wing.span ** 2 * 0.5 * AtmData.dens * AtmData.vel ** 2))
 
         # Range
-        R = (Propeller.eta_P / c_p) / (a1 * a2) * (numpy.arctan(W_initial * a2 / a1) - \
+        R = (Propeller.eta_P / c_p) / (a1 * a2) * (numpy.arctan(W_initial * a2 / a1) -
                                                    numpy.arctan(W_final * a2 / a1))
         # Endurance
         E = R / AtmData.vel
@@ -68,3 +69,50 @@ def propeller_in_cruise(W_initial, W_final, c_p, AtmData, Propeller, Wing, const
     else:  # Unknown error
         print("Error: unknown case_num in assigning based on const_logic, check function!")
         return
+
+
+if __name__ == '__main__':
+    from Class130 import AtmData, Propeller, Wing
+
+
+    g = 9.81    # m/s^2
+    W_initial = 13000  # N
+    W_final = 13000 - (5 * g)  # N
+    W_diff = W_initial - W_final
+
+    # atm data
+    h = 6000 * 0.3048  # m
+    vel = 62  # m/s
+    is_SI = True
+    atm = AtmData(vel, h, is_SI)
+    atm.expand(1.4, 287)
+
+    # prop data
+    eta_P = 0.82
+    prop = Propeller('radius', 'numB', 'RPM', eta_P=eta_P)
+
+    const_logic = [False, True, True]  # [CL, v_inf, h]
+
+    area = 16  # m^2
+    span = 4.70460 * 2  # m
+    CL = (W_initial + W_final) / (atm.dens * atm.vel ** 2 * area)
+
+    # CL = 0.4128
+    CD = 0.018  # from drag polar with no flap, 62 m/s, 6000 ft
+    CD_0 = 0.012
+
+    wing = Wing(area, span, e=0.7, CL=CL, CD=CD, CD_0=CD_0)
+
+    # c_p
+    P_fuelcell_cont = 100 * 1000  # W
+    t_flight_normal = 2200  # s
+    t_flight_divert = 500  # s
+    t_flight_total = t_flight_normal + t_flight_divert
+    Drag = 0.5 * CD * atm.dens * atm.vel**2 * wing.area
+
+    c_p = Drag / (P_fuelcell_cont * t_flight_total)     # N / (W s)
+
+    R, E = propeller_in_cruise(W_initial, W_final, c_p, atm, prop, wing, const_logic)
+
+    print("Range is {:.2f} km".format(R / 1000))
+    print("Endurance is {:.2f} hr".format(E / 3600))
