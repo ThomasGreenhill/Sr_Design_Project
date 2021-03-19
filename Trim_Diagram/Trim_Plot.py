1685cc515b5e64e4c8c682a1f985825b40b135e2
HUI1996HD <59805656+HUI1996HD@users.noreply.github.com>
2021-03-18 21:50:13 -0700

    Add files via upload

---
+++ Trim_Diagram/Trim_Plot.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+import formatfigures
+savedir = os.path.abspath(os.path.dirname(__file__))
+
+alpha_eva = list(map(float, open("Trim_Data_1.txt", "r").read().split()))
+Cmw = list(map(float, open("Trim_Data_2.txt", "r").read().split()))
+Cmh = list(map(float, open("Trim_Data_3.txt", "r").read().split()))
+Cmf = list(map(float, open("Trim_Data_4.txt", "r").read().split()))
+Cm = list(map(float, open("Trim_Data_5.txt", "r").read().split()))
+
+### Reference Line
+R = 0*np.linspace(0,15,100)
+
+### Plot
+plt.figure(figsize=(9,6),num=1)
+plt.plot(alpha_eva,Cmw,label='Wing')
+plt.plot(alpha_eva,Cmh,label='Tail')
+plt.plot(alpha_eva,Cmf,label='Fuselage')
+plt.plot(alpha_eva,Cm,label='Airplane')
+plt.plot(np.linspace(0,15,100),R,'--k')
+plt.xlabel(r'$\alpha (^\circ)$', fontsize=16)
+plt.ylabel(r'$C_{m_\mathrm{CG}}$', fontsize=16)
+plt.xlim(0,12)
+plt.legend(loc="best")
+plt.title('Component contributions to pitching moment about CG')
+formatfigures.formatfigures()
+plt.show()
