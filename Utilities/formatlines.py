f56b60c514c860a3cb29a5bd78a1a3601be66cbe
Xuchang Tang <xuctang@ucdavis.edu>
2021-04-01 17:06:31 -0700

    04.01.2021, XT. improved plot function

--- Utilities/formatlines.py
+++ Utilities/formatlines.py
@@ -1,11 +1,50 @@
-def formatlinestyle(dense=True, loose=False):
+def plot(x_arr, y_arr, count=0, random=False, label=None, linestyle=None, color=None, dense=True, loose=False):
+    """
+    Formats and plots a single line, either in sequence or random
+
+    :param x_arr: (array-like) data set to plot in x-dir
+    :param y_arr: (array-like) data set to plot in y-dir
+    :param count: (int) start with 0, count for recording the line style color number
+    :param random: (bool) True for random line style & colors
+    :param label: (str) label for legend
+    :param linestyle: (str) line style parameter
+    :param color: (str) line color parameter
+    :param dense: (bool) True to enable dense dots and dashes for line style
+    :param loose: (bool) True to enable loose dots and dashes for line style
+    :return count: (int) the count for next function call
+
+    History:
+            Created, XT. 04.01.2021
+            Debugged, XT. 04.01.2021
+    """
+
+    import matplotlib.pyplot as plt
+
+    if linestyle is None:
+        line_style_input = formatlinestyle(count, random, dense, loose)
+    else:
+        line_style_input = linestyle
+
+    if color is None:
+        line_color_input = formatlinecolor(count, random)
+    else:
+
+        line_color_input = color
+
+    plt.plot(x_arr, y_arr, label=label, linestyle=line_style_input, color=line_color_input)
+
+    return count + 1
+
+
+def formatlinestyle(count, random, dense, loose):
     """
     Formats line styles in matplotlib.pyplot
 
     :return: a generated format of plotted line in pyplot
 
     History:
-        Created. XT. 04.01.2021
+        Created, XT. 04.01.2021
+        Debugged, XT. 04.01.2021
     """
 
     line_styles = {
@@ -32,7 +71,6 @@ def formatlinestyle(dense=True, loose=False):
     keys = []
     for item in line_styles.keys():
         keys.append(item)
-    print(keys)
 
     key_ranges = [line_styles['default'][0]]
 
@@ -40,14 +78,18 @@ def formatlinestyle(dense=True, loose=False):
         key_ranges.append(line_styles['default'][1])
 
     if loose:
-        key_ranges.append(line_styles['default'][1])
+        key_ranges.append(line_styles['default'][2])
 
-    key = random_in_ranges(key_ranges)
+    if random:
+        key = random_in_ranges(key_ranges)
+    else:
+        cur_count = count % len(keys)
+        key = keys[cur_count]
 
     return line_styles[key]
 
 
-def formatlinecolor():
+def formatlinecolor(count, random):
     """
     Formats line colors in matplotlib.pyplot
 
@@ -55,11 +97,12 @@ def formatlinecolor():
 
     History:
         Created. XT. 04.01.2021
+        Debugged, XT. 04.01.2021
     """
 
     import random as rd
 
-    line_color = {
+    line_colors = {
         1: 'b',  # blue
         2: 'g',  # green
         3: 'r',  # red
@@ -69,10 +112,26 @@ def formatlinecolor():
         7: 'k'  # black
     }
 
-    line_color.setdefault('default', 'Error in dictionary line_color in formatlinecolor()')
-    key = rd.randint(1, 7)
+    keys = []
+    for item in line_colors.keys():
+        keys.append(item)
 
-    return line_color[key]
+    if random:
+        key = rd.randint(1, 7)
+    else:
+        cur_count = count % len(keys)
+        key = keys[cur_count]
+
+    return line_colors[key]
+
+
+def check_str(input):
+    """
+    Checks if the input is type <str>, raise exception if not
+    :param input: input parameter, expected to be a string
+    """
+    if type(input) is not str:
+        raise Exception('Keywords <label>, <linestyle>, <color> must be strings')
 
 
 def random_in_ranges(ranges, integer=True):
@@ -109,10 +168,12 @@ if __name__ == '__main__':
     x = np.linspace(0, 2 * math.pi, 100)
     y = np.sin(x)
 
-    plt.plot(x, y, linestyle=formatlinestyle(), color=formatlinecolor())
+    plt_count = 0
     for ii in range(10):
-        plt.plot(x, (1 + ii / 10) * y, linestyle=formatlinestyle(), color=formatlinecolor())
+        plt_count = plot(x, (1 + ii / 10) * y, plt_count, random=True, label=str(ii), dense=False, loose=True)
+
     plt.xlabel("x")
     plt.ylabel("sinx")
     plt.title("Sinx Function")
+    plt.legend()
     plt.show()
