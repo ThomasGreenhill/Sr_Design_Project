f60847f692fc1fca3b507d9005a58a6656cdb4df
HUI1996HD <59805656+HUI1996HD@users.noreply.github.com>
2021-04-10 16:30:03 -0700

    Update payload_diagram.py

--- Python_Codes/payload_diagram.py
+++ Python_Codes/payload_diagram.py
@@ -24,7 +24,7 @@ ax1.set_ylabel('Weight (Unit)', color = color1)
 ax1.plot(Range, Woe, color = color1, linestyle='-')  
 ax1.plot(Range, Wp+Woe, color = color1, linestyle='--')  
 ax1.tick_params(axis ='y', labelcolor = color1) 
-ax1.set_xlim([0, np.amax(Range)*1.25])
+ax1.set_xlim([0, np.amax(Range)])
 ax1.set_ylim([0, np.amax(Wp+Woe)*1.25])
 
 #### Notations for Weight vs. Range part
@@ -34,13 +34,13 @@ ax1.arrow(Range[2],-np.amax(Wp+Woe)*0.25,0,np.amax(Wp+Woe)*0.2,length_includes_h
     width=0.02, head_width=0.06, head_length=0.30, color='tab:gray')
 ax1.annotate('Max Payload Range',(Range[1],-np.amax(Wp+Woe)*0.3))
 ax1.annotate('Max Ferry Range',(Range[1],-np.amax(Wp+Woe)*0.3))
-ax1.annotate("",
+ax1.annotate('Operating Empty Weight',
             xy=(Range[1]*0.8, 0), xycoords='data',
             xytext=(Range[1]*0.8,np.amax(Woe)), textcoords='data',
             arrowprops=dict(arrowstyle="<->",
                             connectionstyle="arc3", color=color1, lw=2),
             )
-ax1.annotate("",
+ax1.annotate('Maximum Payload',
             xy=(Range[1]*0.6, np.amin(Wp+Woe)), xycoords='data',
             xytext=(Range[1]*0.6, np.amax(Wp+Woe)), textcoords='data',
             arrowprops=dict(arrowstyle="<->",
@@ -58,18 +58,17 @@ ax2.tick_params(axis ='y', labelcolor = color2)
 ax2.set_ylim([-np.amax(E_b)*1.25, np.amax(E_b)*1.25])
 
 #### Reference Line
-
 ax2.plot(Range, 0*Range, color = 'tab:gray', linestyle='.-') 
 
 
-#### Notations forBattery Charge vs. Range part
-ax2.annotate("",
+#### Notations for Battery Charge vs. Range part
+ax2.annotate('Reverse Charge',
             xy=(Range[1]*1.1, 0), xycoords='data',
             xytext=(Range[1]*1.1,np.amax(E_reserve)), textcoords='data',
             arrowprops=dict(arrowstyle="<->",
                             connectionstyle="arc3", color=color2, lw=2),
             )
-ax2.annotate("",
+ax2.annotate('Battery Charge',
             xy=(Range[1]*1.2, 0), xycoords='data',
             xytext=(Range[1]*1.2, np.amax(E_b)), textcoords='data',
             arrowprops=dict(arrowstyle="<->",
