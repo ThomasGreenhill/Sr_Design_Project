4f26551a873089d6e5711ad330c27b62f0ef7e6a
HUI1996HD <59805656+HUI1996HD@users.noreply.github.com>
2021-04-10 16:01:34 -0700

    Update payload_diagram.py

--- Python_Codes/payload_diagram.py
+++ Python_Codes/payload_diagram.py
@@ -27,6 +27,13 @@ ax1.tick_params(axis ='y', labelcolor = color1)
 ax1.set_xlim([0, np.amax(Range)*1.25])
 ax1.set_ylim([0, np.amax(Wp+Woe)*1.25])
 
+#### Notations for Weight vs. Range part
+ax1.arrow(Range[1],-np.amax(Wp+Woe)*0.25,0,np.amax(Wp+Woe)*0.2,length_includes_head=True,
+    width=0.02, head_width=0.06, head_length=0.30, color='tab:gray')
+ax1.arrow(Range[2],-np.amax(Wp+Woe)*0.25,0,np.amax(Wp+Woe)*0.2,length_includes_head=True,
+    width=0.02, head_width=0.06, head_length=0.30, color='tab:gray')
+ax1.annotate('Max Payload Range',(Range[1],-np.amax(Wp+Woe)*0.3))
+ax1.annotate('Max Ferry Range',(Range[1],-np.amax(Wp+Woe)*0.3))
 
 #### Adding Twin Axes to plot another datasets
 ax2 = ax1.twinx() 
@@ -37,8 +44,6 @@ ax2.plot(Range, E_reserve, color = color1, linestyle='-')
 ax2.plot(Range, E_b, color = color1, linestyle='--') 
 ax2.tick_params(axis ='y', labelcolor = color2) 
 ax2.set_ylim([0, np.amax(E_b)*1.25])
-
-
 
 #### Final Step 
 plt.title('Impact of range on payload of electric aircraft') 
