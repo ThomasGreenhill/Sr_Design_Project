66d37d38e7beb2cac9f4050210e0e70e5b0d4626
michael7452 <michaelpuso@gmail.com>
2021-04-10 15:28:44 -0700

    Old update to CG_locator.py
    4.10.21 MP. Pushing to main since its a pretty small change and don't want to merge anything

--- Python_Codes/CG_locator.py
+++ Python_Codes/CG_locator.py
@@ -2,6 +2,7 @@
 
 from CG_calc import MassObj
 from CG_calc import FindCG
+from CG_calc import Findmassmoment
 import math
 
 def ListAdd(L1, L2):
@@ -57,25 +58,25 @@ V_v = 0.0425
 # Nose tip relative to origin: x = 2.0
 # NOTE: Ignoring y and z locations for now...
 
-WingRootLoc = [5, 0, 0] # Location of Wing Root LE
+WingRootLoc = [5, 0, 2+0.59] # Location of Wing Root LE
 HTRootLoc = 9.75
 VTRootLoc = 9.75
 
-CML = [2.4, 0, 0]         # Canard motor location
-CMCL = [2.4-0.0762, 0, 0] # Canard motor controller location
-CRL = [2.8, 0, 0]         # Canard rotor location
+CML = [2.4, 1.3, 2-0.15]         # Canard motor location
+CMCL = [2.4-0.0762, 1.3, 2-0.15] # Canard motor controller location
+CRL = [2.8, 1.3, 2-0.15]         # Canard rotor location
 
-IMRL = [1.448, 0, 0] # Inboard motor location relative to WingRootLoc
-MMRL = [1.503, 0, 0] # Middle   "                                   "
-OMRL = [1.559, 0, 0] # Outboard "                                   "
+IMRL = [1.448, 1.62, 0] # Inboard motor location relative to WingRootLoc
+MMRL = [1.503, 3.77, 0] # Middle   "                                   "
+OMRL = [1.559, 5.915, 0] # Outboard "                                   "
 
-IMCRL = [1.448-0.0762, 0, 0] # Inboard motor controller location relative to WingRootLoc
-MMCRL = [1.503-0.0762, 0, 0] # Middle   "                                              "
-OMCRL = [1.559-0.0762, 0, 0] # Outboard "                                              "
+IMCRL = [1.448-0.0762, 1.62, 0] # Inboard motor controller location relative to WingRootLoc
+MMCRL = [1.503-0.0762, 3.77, 0] # Middle   "                                              "
+OMCRL = [1.559-0.0762, 5.915, 0] # Outboard "                                              "
 
-IRRL = [1.448+0.4, 0, 0] # Inboard rotor location relative to WingRootLoc
-MRRL = [1.503+0.4, 0, 0] # Middle   "                                   "
-ORRL = [1.559+0.4, 0, 0] # Outboard "                                   "
+IRRL = [1.448+0.4, 1.62, 0] # Inboard rotor location relative to WingRootLoc
+MRRL = [1.503+0.4, 3.77, 0] # Middle   "                                   "
+ORRL = [1.559+0.4, 5.915, 0] # Outboard "                                   "
 
 # ================================================#
 #####     Calculate Main Wing CG Location     #####
@@ -102,7 +103,7 @@ WingRelXAC = WingRelYAC*((0.8*RootChord + Span*math.sin(SweepAngle) - 0.8*TipCho
 # ================================================#
 
 # Fuselage
-Fuselage = MassObj(152.3*0.7, 5.51, 0, 0)
+Fuselage = MassObj(152.3*0.7, 5.51, 0, 0.7)
 
 # Main Wing
 MainWing = MassObj(240*0.7,*ListAdd(WingRootLoc, WingRelCG))
@@ -110,12 +111,12 @@ MainWing = MassObj(240*0.7,*ListAdd(WingRootLoc, WingRelCG))
 # Empennage (initial guess, will be refined)
 HorzTailArea = 4.6
 VertTailArea = 2.098
-HorzTail = MassObj(46*0.8, HTRootLoc+RelHorzTailCG_X(HorzTailArea), 0, 0)
-VertTail = MassObj(20.98*0.8, VTRootLoc+RelVertTailCG_X(VertTailArea), 0, 0)
+HorzTail = MassObj(46*0.8, HTRootLoc+RelHorzTailCG_X(HorzTailArea), 0, 2.15)
+VertTail = MassObj(20.98*0.8, VTRootLoc+RelVertTailCG_X(VertTailArea), 0, 2.5)
 Empennage = [HorzTail, VertTail]
 
 # Canard
-Canard = MassObj(8.45*0.7, 3.32675, 0, 0)
+Canard = MassObj(8.45*0.7, 3.32675, 0, 2-0.15)
 
 # Motors
 CanardMotor   = MassObj(12*2, *CML)
@@ -139,36 +140,36 @@ OutboardRotor = MassObj(2, *ListAdd(WingRootLoc, ORRL))
 RotorList = [CanardRotor, InboardRotor, MiddleRotor, OutboardRotor]
 
 # Passengers+Seats
-FrontPax  = MassObj(81.65, 4.205, 0, 0)
-FrontSeat = MassObj(6.804, 4.205, 0, 0)
-RearPax1  = MassObj(81.65, 5.1874, 0, 0)
-RearPax2  = MassObj(81.65, 5.1874, 0, 0)
-RearSeats = MassObj(6.804*2, 5.1874, 0, 0)
+FrontPax  = MassObj(81.65, 4.205, 0, 2-0.03)
+FrontSeat = MassObj(6.804, 4.205, 0, 2-0.3)
+RearPax1  = MassObj(81.65, 5.1874, 0.3, 2-0.09)
+RearPax2  = MassObj(81.65, 5.1874, 0.3, 2-0.09)
+RearSeats = MassObj(6.804*2, 5.1874, 0, 2-0.36)
 Pax   = [FrontPax, RearPax1, RearPax2]
 Seats = [FrontSeat, RearSeats]
 
 # Landing Gear
-FrontGear = MassObj(0.057*1300*0.15, 4, 0, 0)
-RearGears = MassObj(0.057*1300*0.85, 6.5, 0, 0)
+FrontGear = MassObj(0.057*1300*0.15, 4, 0, 1.5)
+RearGears = MassObj(0.057*1300*0.85, 6.5, 0.25, 1.5)
 LandingGear = [FrontGear, RearGears]
 
 # Luggage
-Luggage = MassObj(25, 2.5, 0, 0)
+Luggage = MassObj(25, 2.5, 0, 2)
 
 # Fixed Equipment
-FixedEquip = MassObj(1300*0.05, 5, 0, 0)
+FixedEquip = MassObj(1300*0.05, 5, 0, 2.3)
 
 # Fuel Cells
-FuelCell = MassObj(385*0.75, 6, 0, 0)
+FuelCell = MassObj(385*0.75, 6, 0, 2-0.23)
 FuelCells = [FuelCell]
 
 # Fuel Tank and Fuel
-FuelTank = MassObj(5*9, 6, 0, 0)
-FuelMass = MassObj(5, FuelTank.x, 0, 0)
+FuelTank = MassObj(5*9, 6, 0, 2+0.3)
+FuelMass = MassObj(5, FuelTank.x, 0, 2.3)
 FuelTotal = [FuelTank, FuelMass]
 
 # Battery
-Battery = MassObj(35000/470, 4.5, 0, 0)
+Battery = MassObj(35000/470, 4.5, 0, 2-0.58)
 
 objs = [Fuselage, *MotorList, *MotorConList, *RotorList, MainWing, *Empennage,\
         Canard, *LandingGear, *Pax, *Seats, Luggage, FixedEquip, *FuelCells,\
@@ -186,14 +187,14 @@ for i in range(10):
         x_v = VTRootLoc + VertTail_XAC(VertTailArea) - CG_pos[1]
         HorzTailArea = V_h*S*c/x_h
         VertTailArea = V_v*S*b/x_v
-    HorzTail = MassObj(10*HorzTailArea*0.7, HTRootLoc+RelHorzTailCG_X(HorzTailArea), 0, 0)
-    VertTail = MassObj(10*VertTailArea*0.7, VTRootLoc+RelVertTailCG_X(VertTailArea), 0, 0)
+    HorzTail = MassObj(10*HorzTailArea*0.7, HTRootLoc+RelHorzTailCG_X(HorzTailArea), 0, 2.15)
+    VertTail = MassObj(10*VertTailArea*0.7, VTRootLoc+RelVertTailCG_X(VertTailArea), 0, 2.5)
     Empennage = [HorzTail, VertTail]
     objs = [Fuselage, *MotorList, *MotorConList, *RotorList, MainWing, *Empennage,\
             Canard, *LandingGear, *Pax, *Seats, Luggage, FixedEquip, *FuelCells,\
             *FuelTotal, Battery]
     CG_pos = FindCG(objs)
-    Im = Findmassmoment(objs) ## Changed by Yihui, 03/18/2021
+Im = Findmassmoment(objs) ## Changed by Yihui, 03/18/2021
     
 print("Total Mass: {:4f} kg".format(CG_pos[0]))
 print("Horizontal Tail Area: {:.4f}".format(HorzTailArea))
@@ -202,7 +203,9 @@ print("xCOM: {:4f} m".format(CG_pos[1]))
 print("xCOL approximate: {:.4f} m".format(FindCG(RotorList)[1]))
 print("xCOL as proportion of chord past wing root LE: {:.4f}".format((FindCG(RotorList)[1]-WingRootLoc[0])/AveChord))
 print("xCOM as proportion of chord past wing root LE: {:.4f}".format((CG_pos[1]-WingRootLoc[0])/AveChord))
-print("Mass Moment of Inertia: %4f kg-m^2"%(Im)) ## Changed by Yihui, 03/18/2021
+print("Mass Moment of Inertia about x: {:.4f} kg-m^2".format(Im[0])) ## Changed by Yihui, 03/18/2021
+print("Mass Moment of Inertia about y: {:.4f} kg-m^2".format(Im[1])) ## Changed by Yihui, 03/18/2021
+print("Mass Moment of Inertia about z: {:.4f} kg-m^2".format(Im[2])) ## Changed by Yihui, 03/18/2021
 
 print("\nCalculating CG based on empy weight:")
 print("------------------------------------")
