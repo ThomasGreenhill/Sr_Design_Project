289e9fb3e1a1610fc9665bb182d7a2ed9d4ed5d8
HUI1996HD <59805656+HUI1996HD@users.noreply.github.com>
2021-06-05 16:05:04 -0700

    Add files via upload

---
+++ Flutter_Analysis/Flutter_Analysis_Plots.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+savedir = os.path.abspath(os.path.dirname(__file__))
+
+#### Extract information from txt file
+rawfile = open('%s\\wing_dd_flutter.txt'%(savedir), 'r') # Open the entire file
+rawinfo = rawfile.read()         # Read the entire file to string
+rawfile.close()         # Close the entire file
+rawinfo = rawinfo.split('\n')
+
+
+
+#### Loop for separating the group and changing into usable list
+flutter_list = {}
+j = 1
+for i in range(len(rawinfo)):    
+    if 'FLUTTER  SUMMARY' in rawinfo[i]:
+        X = [x.split() for x in rawinfo[(i+5):(i+17)]]
+        flutter_list["group_%s"%(j)] = pd.DataFrame(data = [list(map(float, i)) for i in X[1:]], columns = X[0])
+        j += 1
+
+
+#### Make plot (Frequnecy vs Speed)
+ax1 = plt.subplot()
+for i in range(1, j):  
+    flutter_list["group_%s"%(i)].plot(kind='line', x='VELOCITY', y='KFREQ', ax = ax1, label = 'point %s' % i)
+ax1.set_xlabel('Velocity (m/s)')
+ax1.set_ylabel('Frequency (Hz)')
+ax1.set_title('Frequency vs Speed')
+plt.show()
+
+#### Make plot (Damping vs Speed)
+
+ax2 = plt.subplot()
+for i in range(1, j):  
+    flutter_list["group_%s"%(i)].plot(kind='line', x='VELOCITY', y='DAMPING', ax = ax2, label = 'point %s' % i)
+ax2.set_xlabel('Velocity (m/s)')
+ax2.set_ylabel('Damping')
+ax2.set_title('Damping vs Speed')
+plt.show()
